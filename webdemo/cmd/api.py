cf65d72482b5b6b7696ac219f93b06a450d6534f
bran-wang <branw@vmware.com>
2017-04-04 03:03:47 +0800

    setup app

---
+++ webdemo/cmd/api.py
@@ -0,0 +1,16 @@
+from wsgiref import simple_server
+
+from webdemo.api import app
+
+def main():
+    host = "0.0.0.0"
+    port = 8089
+
+    application = app.setup_app()
+    srv = simple_server.make_server(host, port, application)
+
+    srv.server_forever()
+
+
+if __name__ == '__main__':
+    main()
